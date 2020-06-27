7a3b11d93cf4d96e6d954870174764b19851e40a
plastr <37421+plastr@users.noreply.github.com>
2020-06-27 14:46:39 -0700

    Use node_modules above code inside container to avoid symlinks.

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,1 @@
 docker-compose build --force-rm --no-cache
-echo "Installing symlinks for node_modules"
-ls -a
-ls -a server
-# ln -sv /tmp/dashboard-install/node_modules ./../dashboard/node_modules
-ln -sv /tmp/server-install/node_modules server/node_modules
