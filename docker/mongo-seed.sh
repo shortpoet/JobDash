6cd4e2de6f034fd2e6e4815e6fa7a756ea82f82f
plastr <37421+plastr@users.noreply.github.com>
2020-06-28 12:21:46 -0700

    Kill the entrypoint too

--- docker/mongo-seed.sh
+++ docker/mongo-seed.sh
@@ -1,2 +1,2 @@
 # Mounts the server/seeds files into /opt/seed in the container
-docker-compose run --rm -v "${PWD}"/server/seed:/opt/seed mongo /opt/seed/import-seeds.sh "$@"
+docker-compose run --rm --entrypoint bash -v "${PWD}"/server/seed:/opt/seed mongo /opt/seed/import-seeds.sh "$@"
