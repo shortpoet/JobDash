32463cd5499244d82fa67bfcb2381dbf4e542f26
plastr <37421+plastr@users.noreply.github.com>
2020-06-28 11:50:29 -0700

    See if this improves running on Windows

--- server/seed/import-seeds.sh
+++ server/seed/import-seeds.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mongoimport --host mongo --db job-db --drop --collection contacts --type json --file /opt/seed/contacts.seed.json --jsonArray
 mongoimport --host mongo --db job-db --drop --collection tasks --type json --file /opt/seed/tasks.seed.json --jsonArray
 mongoimport --host mongo --db job-db --drop --collection messages --type json --file /opt/seed/messages.seed.json --jsonArray
