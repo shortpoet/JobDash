ecf92f93bc3de2e0eb2c9b07783e768ca37fae70
plastr <37421+plastr@users.noreply.github.com>
2020-06-28 13:32:23 -0700

    This should now work if the CRLF issue has been resolved

--- docker/mongo-seed.sh
+++ docker/mongo-seed.sh
@@ -1,2 +1,2 @@
 # Mounts the server/seeds files into /opt/seed in the container
-docker-compose run --rm mongo sh -c /opt/seed/import-seeds.sh
+docker-compose run --rm mongo /opt/seed/import-seeds.sh
--- server/seed/import-seeds.sh
+++ server/seed/import-seeds.sh
@@ -1,6 +1,8 @@
-# SCRIPT="$(cd "${0%/*}" && echo "$PWD"/${0##*/})"
-# BASEDIR="`dirname "$SCRIPT"`"
-# cd $BASEDIR
+#!/bin/bash
+set -o errexit -o pipefail
+SCRIPT="$(cd "${0%/*}" && echo "$PWD"/${0##*/})"
+BASEDIR="`dirname "$SCRIPT"`"
+cd $BASEDIR
 
 mongoimport --host mongo --db job-db --drop --collection contacts --type json --file contacts.seed.json --jsonArray
 mongoimport --host mongo --db job-db --drop --collection tasks --type json --file tasks.seed.json --jsonArray
