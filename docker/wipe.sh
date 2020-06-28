fc750072b30a1eaceea4a3f495b83d600b50e3f9
plastr <37421+plastr@users.noreply.github.com>
2020-06-28 10:00:32 -0700

    Unix style newlines

--- docker/wipe.sh
+++ docker/wipe.sh
@@ -1,4 +1,4 @@
 docker-compose down --rmi local --remove-orphans --volumes
 docker image rm shortpoet/jobdash-client
-docker image rm shortpoet/jobdash-mongo-seed 
 docker image rm shortpoet/jobdash-server
+docker image rm shortpoet/jobdash-mongo-seed
