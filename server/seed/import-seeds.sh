22fcf7ed36701d199cc418d4b653e87eaaae1f37
plastr <37421+plastr@users.noreply.github.com>
2020-06-28 16:55:10 -0700

    Remove this bit of confusion

--- server/seed/import-seeds.sh
+++ server/seed/import-seeds.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -o errexit -o pipefail
-SCRIPT="$(cd "${0%/*}" && echo "$PWD"/${0##*/})"
-BASEDIR="`dirname "$SCRIPT"`"
-cd $BASEDIR
-
 mongoimport --host mongo --db job-db --drop --collection contacts --type json --file contacts.seed.json --jsonArray
 mongoimport --host mongo --db job-db --drop --collection tasks --type json --file tasks.seed.json --jsonArray
 mongoimport --host mongo --db job-db --drop --collection messages --type json --file messages.seed.json --jsonArray
